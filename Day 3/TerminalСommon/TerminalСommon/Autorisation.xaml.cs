592a773b7d2561b10626240343341b3f6b789e85
ALTERRA4546 <kordroy20@mail.ru>
2024-04-20 13:52:11 +0300

    Day 3
    1. Настольное приложение для посетителя.
    2. настольное приложение "Терминал сотрудника общего отдела".
    3. Представление ViewListRequests (sql-код).
    4. Хранимая процедура FilteringRequests (sql-код).

---
+++ Day 3/TerminalСommon/TerminalСommon/Autorisation.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace TerminalСommon
+{
+    /// <summary>
+    /// Логика взаимодействия для Autorisation.xaml
+    /// </summary>
+    public partial class Autorisation : Window
+    {
+        public Autorisation()
+        {
+            InitializeComponent();
+        }
+
+        private void EnterB_Click(object sender, RoutedEventArgs e)
+        {
+            int kode = Convert.ToInt32(login.Text);
+            using (var bd = new HranitelProEntities())
+            {
+                var otdel = bd.Отдел.FirstOrDefault(s=>s.Название == "Общий отдел");
+                var sotrudnik = bd.Сотрудник.Where(s => s.Код_отдела == otdel.Код_отдела).FirstOrDefault(s=>s.Код_сотрудника == kode);
+                if (sotrudnik != null)
+                {
+                    ViewOrder vo = new ViewOrder();
+                    vo.Show();
+                    this.Hide();
+                }
+            }
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
---
+++ Day 3/TerminalСommon/TerminalСommon/ViewOrder.xaml.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace TerminalСommon
+{
+    /// <summary>
+    /// Логика взаимодействия для ViewOrder.xaml
+    /// </summary>
+    public partial class ViewOrder : Window
+    {
+        public ViewOrder()
+        {
+            InitializeComponent();
+            Initial();
+        }
+
+        public static class Zayavka
+        { 
+            public static int kodZayavky;
+        }
+
+        private void Initial()
+        {
+            using (var bd = new HranitelProEntities())
+            {
+                var ty = bd.Тип_заявки.Select(s=>s.Название).ToList();
+                var pd = bd.Подразделение.Select(s => s.Назавние).ToList();
+                var st = bd.Статус.Select(s => s.Название).ToList();
+                ty.Add("Все");
+                pd.Add("Все");
+                st.Add("Все");
+                type.ItemsSource = ty;
+                podrazd.ItemsSource = pd;
+                status.ItemsSource = st;
+                type.SelectedItem = "Все";
+                podrazd.SelectedItem = "Все";
+                status.SelectedItem = "Все";
+
+                var zayavka = from z in bd.Заявка
+                              join t in bd.Тип_заявки on z.Код_типа_заявки equals t.Код_типа_заявки
+                              join s in bd.Статус on z.Код_статуса equals s.Код_cтатуса
+                              join prp in bd.Пропуск on z.Код_пропуска equals prp.Код_пропуска
+                              join pdr in bd.Подразделение on z.Код_подразделения equals pdr.Код_подразделения
+                              join pos in bd.Посещающие on z.Код_заявки equals pos.Код_заявки
+                              join sotr in bd.Сотрудник on z.Код_сотрудника equals sotr.Код_сотрудника
+                              join poset in bd.Посетитель on pos.Код_посетителя equals poset.Код_посетителя
+                              join org in bd.Организация on poset.Код_организации equals org.Код_организации
+                              select new
+                              {
+                                  z.Код_заявки,
+                                  Тип_заявки = t.Название,
+                                  Статус = s.Название,
+                                  Подразделение = pdr.Назавние,
+                                  sotr.Код_сотрудника,
+                                  prp.Код_пропуска,
+                                  prp.Срок_начала_действия,
+                                  prp.Срок_окончания_действия,
+                                  prp.Цель_посещения,
+                                  z.Дата_создания,
+                                  z.Дата_и_время_посящения
+                              };
+                dataOut.ItemsSource = zayavka.ToList();
+            }
+        }
+
+        private void Filter(string tp, string pod, string stat)
+        {
+            string t = null;
+            string p = null;
+            string s = null;
+            if (tp != "Все")
+                t = tp;
+            if (pod != "Все")
+                p = pod;
+            if (stat != "Все")
+                s = stat;
+            Filtration(t, p, s);
+        }
+
+        private void Filtration(string tp, string pod, string stat)
+        {
+            using (var bd = new HranitelProEntities())
+            {
+                var zayavka = from z in bd.Заявка
+                              join t in bd.Тип_заявки on z.Код_типа_заявки equals t.Код_типа_заявки
+                              join s in bd.Статус on z.Код_статуса equals s.Код_cтатуса
+                              join prp in bd.Пропуск on z.Код_пропуска equals prp.Код_пропуска
+                              join pdr in bd.Подразделение on z.Код_подразделения equals pdr.Код_подразделения
+                              join pos in bd.Посещающие on z.Код_заявки equals pos.Код_заявки
+                              join sotr in bd.Сотрудник on z.Код_сотрудника equals sotr.Код_сотрудника
+                              join poset in bd.Посетитель on pos.Код_посетителя equals poset.Код_посетителя
+                              join org in bd.Организация on poset.Код_организации equals org.Код_организации
+                              where((tp == null || t.Название == tp) && (pod == null || pdr.Назавние == pod) && (stat == null || s.Название == stat))
+                              select new
+                              {
+                                  z.Код_заявки,
+                                  Тип_заявки = t.Название,
+                                  Статус = s.Название,
+                                  Подразделение = pdr.Назавние,
+                                  sotr.Код_сотрудника,
+                                  prp.Код_пропуска,
+                                  prp.Срок_начала_действия,
+                                  prp.Срок_окончания_действия,
+                                  prp.Цель_посещения,
+                                  z.Дата_создания,
+                                  z.Дата_и_время_посящения
+                              };
+                dataOut.ItemsSource = zayavka.ToList();
+            }
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void select_Click(object sender, RoutedEventArgs e)
+        {
+            if (dataOut.SelectedIndex < 0)
+            {
+                MessageBox.Show("Строка не была выбрана");
+                return;
+            }
+            DataGridRow row = (DataGridRow)dataOut.ItemContainerGenerator.ContainerFromIndex(dataOut.SelectedIndex);
+            DataGridCell cell = dataOut.Columns[0].GetCellContent(row).Parent as DataGridCell;
+            Zayavka.kodZayavky = Convert.ToInt32(((TextBlock)cell.Content).Text);
+            CheckOrder co = new CheckOrder();
+            co.ShowDialog();
+        }
+
+        private void type_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (type.SelectedIndex < 0 || podrazd.SelectedIndex < 0 || status.SelectedIndex < 0)
+            {
+                return;
+            }
+            Filter(type.SelectedItem.ToString(), podrazd.SelectedItem.ToString(), status.SelectedItem.ToString());
+        }
+    }
+}
